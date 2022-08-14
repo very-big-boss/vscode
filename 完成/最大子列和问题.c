154a4f5a12530beb4bd08aa2bd7a43344673a602
bao <b13987609986339@icloud.com>
2022-08-14 20:39:30 +0800

    1

---
+++ 完成/最大子列和问题.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int x,a[100000]={0},sum=0;
+void sss(int o){
+    if(o<=x-1){
+        int z=a[o];
+        if(z>sum)sum=z;
+        for(int i=o+1;i<=x-1;i++){
+            z+=a[i];
+            if(z>sum)sum=z;
+        }
+        sss(o+1);
+    }
+    else return;
+}
+int main(void){
+    scanf("%d",&x);
+    for(int i=0;i<x;i++){
+        scanf("%d",&a[i]);
+    }
+    sss(0);
+    printf("%d\n",sum);
+    return 0;
+}
