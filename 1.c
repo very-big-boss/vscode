daa8a4eaabccbeb26c19beeed55f8895f1316f71
bao <b13987609986339@icloud.com>
2022-08-30 16:46:44 +0800

    1

--- 1.c
+++ 1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+#include<math.h>
+_Bool ab(int n){
+    if(n==1)return 0;
+    for(int i=2;i<=sqrt(n);i++)if(n%i==0)return 0;
+    return 1;
+}
 int main(void){
-    
+    for(int i=1;i<=100;i++)if(ab(i)==1)printf("%d\n",i);
     return 0;
 }
--- 完成/符号沙漏.c
+++ 完成/符号沙漏.c
@@ -2,26 +2,19 @@
 #include<math.h>
 char a;int x;int v;
 void kkk(int n){
-    for(int i=0;i<(v-n);i++){
-        printf(" ");
-    }
-    for(int i=0;i<(2*n-1);i++){
-        printf("%c",a);
-    }printf("\n");
+    for(int i=0;i<(v-n);i++)printf(" ");
+    for(int i=0;i<(2*n-1);i++)printf("%c",a);
+    printf("\n");
     if(n!=1){
        kkk(n-1);
-       for(int i=0;i<(v-n);i++){
-            printf(" ");
-        }
-        for(int i=0;i<(2*n-1);i++){
-            printf("%c",a);
-        }printf("\n"); 
+       for(int i=0;i<(v-n);i++)printf(" ");
+        for(int i=0;i<(2*n-1);i++)printf("%c",a);
+        printf("\n"); 
     }
-    
 }
 int main(void){
     scanf("%d %c",&x,&a);
-    int n=pow((x+1)/2,0.5);v=n;
+    int n=sqrt((x+1)/2);v=n;
     kkk(n);
     printf("%d\n",x-(2*n*n-1));
     return 0;
